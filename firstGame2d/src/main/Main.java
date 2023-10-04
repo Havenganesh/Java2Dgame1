d184b321cf3ee010ad42e471445c52fda4107614
Ganesh <ganesh@ganesh-Vostro-15-3568>
2023-10-04 22:08:57 +0530

    initial commit

---
+++ firstGame2d/src/main/Main.java
@@ -0,0 +1,16 @@
+package main;
+
+import javax.swing.JFrame;
+
+public class Main {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.setTitle("Ganesh 2D game");
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+		
+	}
+
+}
